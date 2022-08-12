ba980c05a39a60bbf708c452ecd1ef275a4740d2
Mr.V <dzulajvita@gmail.com>
2022-08-12 12:13:08 +0300

    layout: article cards, cart

--- src/App.js
+++ src/App.js
@@ -1,20 +1,73 @@
 function App() {
   return (
     <div className="wrapper clear">
+      <div className="overlay">
+        <div className="drawer">
+          <h2 className="mb-30">Корзина</h2>
+
+          <div className="items">
+            <div className="cartItem mb-20 d-flex align-center">
+              <div className="cartItemImg"
+                  style={{backgroundImage: "url('img/sneakers/1.jpg')"}}></div>
+
+              <div className="mr-20">
+                <p className="mb-5">Чоловічі кросівки Nike Blazer Mid Suede</p>
+                <b>4 333 грн.</b>
+              </div>
+
+              <img className="removeBtn" src="img/remove.svg" alt="" />
+            </div>
+
+            <div className="cartItem mb-20 d-flex align-center">
+              <div className="cartItemImg"
+                  style={{backgroundImage: "url('img/sneakers/1.jpg')"}}></div>
+
+              <div className="mr-20">
+                <p className="mb-5">Чоловічі кросівки Nike Blazer Mid Suede</p>
+                <b>4 333 грн.</b>
+              </div>
+
+              <img className="removeBtn" src="img/remove.svg" alt="" />
+            </div>
+          </div>
+
+          <div className="cartTotalBlock">
+            <ul>
+              <li>
+                <span>Всього: </span>
+                <div></div>
+                <b>10 000 грн.</b>
+              </li>
+
+              <li>
+                <span>Податок 20%: </span>
+                <div></div>
+                <b>2000 грн.</b>
+              </li>
+            </ul>
+
+            <button>
+              Створити замовлення
+              <img src="img/arrow.svg" alt="" />
+            </button>
+          </div>
+        </div>
+      </div>
+
       <header className="d-flex justify-between align-center p-40">
         <div className="d-flex align-center">
           <img src="img/logo.png" alt="" width={40} height={40}/>
           
           <div>
             <h3 className="text-uppercase">React Sneakers</h3>
-            <p className="opacity-5">Shop of the best sneakers</p>
+            <p className="opacity-5">Магазин найкращих кросівок</p>
           </div>
         </div>
 
         <ul className="d-flex">
           <li className="mr-20">
             <img src="img/cart.svg" alt="" width={18} height={18}/>
-            <span>$300</span>
+            <span>4 333 грн.</span>
           </li>
 
           <li>
@@ -24,17 +77,28 @@ function App() {
       </header>
 
       <div className="content p-40">
-        <h1 className="mb-40">All sneakers</h1>
+        <div className="d-flex align-center justify-between mb-40">
+          <h1 className="">Всі кросівки</h1>
+          
+          <div className="searchBlock d-flex">
+            <img src="img/search.svg" alt="" />
+            <input type="text" placeholder="Search..." />
+          </div>  
+        </div>
 
         <div className="d-flex justify-between">
           <div className="card">
+            <div className="favorite">
+              <img src="img/unliked.svg" alt="" />
+            </div>
+            
             <img src="img/sneakers/1.jpg" alt="" width={133} height={112}/>
-            <p>Male Sneakers Nike Blazer Mid Suede</p>
+            <p>Чоловічі кросівки Nike Blazer Mid Suede</p>
 
             <div className="d-flex justify-between align-center">
               <div className="d-flex flex-column">
-                <span>Price: </span>
-                <b>$279</b>
+                <span>Ціна: </span>
+                <b>4 333 грн.</b>
               </div>
 
               <button className="button">
@@ -45,12 +109,12 @@ function App() {
 
           <div className="card">
             <img src="img/sneakers/2.jpg" alt="" width={133} height={112}/>
-            <p>Male Sneakers Nike Blazer Mid Suede</p>
+            <p>Чоловічі кросівки Nike Blazer Mid Suede</p>
 
             <div className="d-flex justify-between align-center">
               <div className="d-flex flex-column">
-                <span>Price: </span>
-                <b>$279</b>
+                <span>Ціна: </span>
+                <b>4 333 грн.</b>
               </div>
 
               <button className="button">
@@ -61,12 +125,12 @@ function App() {
 
           <div className="card">
             <img src="img/sneakers/3.jpg" alt="" width={133} height={112}/>
-            <p>Male Sneakers Nike Blazer Mid Suede</p>
+            <p>Чоловічі кросівки Nike Blazer Mid Suede</p>
 
             <div className="d-flex justify-between align-center">
               <div className="d-flex flex-column">
-                <span>Price: </span>
-                <b>$279</b>
+                <span>Ціна: </span>
+                <b>4 333 грн.</b>
               </div>
 
               <button className="button">
@@ -77,12 +141,12 @@ function App() {
 
           <div className="card">
             <img src="img/sneakers/4.jpg" alt="" width={133} height={112}/>
-            <p>Male Sneakers Nike Blazer Mid Suede</p>
+            <p>Чоловічі кросівки Nike Blazer Mid Suede</p>
 
             <div className="d-flex justify-between align-center">
               <div className="d-flex flex-column">
-                <span>Price: </span>
-                <b>$279</b>
+                <span>Ціна: </span>
+                <b>4 333 грн.</b>
               </div>
 
               <button className="button">
